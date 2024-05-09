0f268ee9536851d142d81f0c2180841a5782a95e
Gabrielle Lommel <granolagabrielle@gmail.com>
2024-05-09 16:52:32 -0500

    Completed some base goals

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,69 @@
 function onReady() {
-  console.log("JavaScript is loaded!")
+  console.log('JavaScript is loaded!');
+  fetchGuesses();
 }
 
-onReady()
+onReady();
+let guessesMade = document.getElementById('guesses-made');
+
+
+function guessHandler(event) {
+  event.preventDefault();
+
+  const playerName = document.getElementById('player-name').value;
+  const playerGuess = document.getElementById('player-guess').value;
+
+  axios({
+    method: 'POST',
+    url: '/guesses',
+    data: { playerName, playerGuess },
+  })
+    .then(function (response) {
+      console.log('Guess has been accepted!');
+      fetchGuesses();
+
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert('Something is not right, check the console');
+    });
+  document.getElementById('player-name').value = '';
+  document.getElementById('player-guess').value = '';
+}
+
+function fetchGuesses() {
+  axios
+    .get('/guesses')
+    .then((response) => {
+      const tableBody = document.getElementById('guessHistoryTable');
+      tableBody.innerHTML = '';
+      for (let i = 0; i < response.data.length; i++) {
+        let guess = response.data[i];
+        tableBody.innerHTML += `
+              <tr>
+                  <td>${guess.playerName}</td>
+                  <td>${guess.playerGuess}</td>
+                  <td>${guess.result}</td>
+              </tr>
+          `;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      alert('Something is not right, check the console.');
+    });
+}
+
+function guessCounter(){
+  axios.get('/guessesMade').then(response => {
+    const guessesMade = document.getElementById('guesses-made');
+    console.log(response.data)
+    guessesMade.value = response.data;
+ 
+  }).catch(error => {
+    console.log(error);
+    alert('Something went wrong, check the console')
+  })
+}
+
+// setInterval(fetchGuesses, 1000);
--- server/server.js
+++ server/server.js
@@ -1,17 +1,74 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+// array to hold guesses
+let randomNumber = Math.floor(Math.random() * 25) + 1;
+let numberGuesses = [];
+let guessesMade = 0;
+
 // GET & POST Routes go here
 
+// POST for submitting guesses to server
+app.post('/guesses', (req, res) => {
+  let guess = req.body;
+  numberGuesses.push(guess);
+  res.status(201).send(numberGuesses);
+});
+
+// POST for guess counter
+// app.post('/guessesMade', (req, res) => {
+//   guessesMade++;
+//   res.status(201).send(guessesMade);
+// });
+
+app.get('/guessesMade', (req, res) => {
+  guessesMade++;
+  res.send(guessesMade);
+});
+
+// GET to retrieve all previous guesses
+app.get('/guesses', (req, res) => {
+  let resultArray = [];
+  // let result = {};
+  // const tooHigh = [];
+  // const tooLow = [];
+
+  console.log(randomNumber);
+
+  for (const guess of numberGuesses) {
+    if (guess.playerGuess === randomNumber) {
+      guess.result = 'Correct!';
+      resultArray.push(guess);
+      const restartButton = document.getElementById('restart-button');
+      restartButton.innerHTML = `<button>Restart</button>`;
+    } else if (guess.playerGuess > randomNumber) {
+      guess.result = 'Too high!';
+
+      // result = { name: guess.name, guess: guess.guess, result: 'Too high!' };
+      resultArray.push(guess);
+    } else {
+      guess.result = 'Too Low!';
+
+      // result = { name: guess.name, guess: guess.guess, result: 'Too Low' };
+      resultArray.push(guess);
+    }
+    console.log(guess);
+  }
+  console.log(resultArray);
+
+  res.send(resultArray);
+});
+
+// app.delete ('/guesses')
 
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
-})
+  console.log('Server is running on port', PORT);
+});
